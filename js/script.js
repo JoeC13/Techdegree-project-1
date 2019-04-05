2c5644cc1a8f5299eae780b6155ab1bae657e45b
Joseph Cortez <jcortez1322@gmail.com>
2019-04-04 23:54:48 -0400

    js code w/ comments

--- js/script.js
+++ js/script.js
@@ -3,54 +3,91 @@ Treehouse FSJS Techdegree:
 project 1 - A Random Quote Generator
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
+/***
+The following code is the final product for unit-1, Random Quote Generator created by the treehouse student, Joseph Cortez
+and submitted for grading.  This code also includes an attempt at the 'Exceeds Expectations' portion. Thank You!
 ***/
+
+let quotes = [
+  {
+    quote: 'There may be people that have more talent than you, but theres no excuse for anyone to work harder than you do.',
+    source: 'Derek Jeter',
+    tag: 'Sports'
+  },
+  {
+    quote: 'Accept the challenges so that you can feel the exhilaration of victory.',
+    source: 'George S. Patton',
+    tag: 'Military'
+  },
+  {
+    quote: 'Rice is great when you\'re hungry and you want 2000 of something',
+    source: 'Mitch Hedberg',
+    tag: 'Humor'
+  },
+  {
+    quote: 'You almost had me? You never had me. You never had your car',
+    source: 'Vin Diesel',
+    citation: 'The Fast and the Furious',
+    year: '2001',
+    tag: 'Movies'
+  },
+  {
+    quote: 'Yesterday\'s homeruns don\'t win today\'s games',
+    source: 'Babe Ruth',
+    tag: 'Sports'
+  }
+];
+
+//An array of colors for random color function.
+var bgColor = ['#000066', '#99ffcc', '#cbbeb', '#00ff7f', '#ff4040',
+'#494f52', '#c3304a'];
+
+//Random number function for the  quotes array of objects
+function getRandomQuote(){
+  var i = Math.floor(Math.random() * quotes.length);
+  return quotes[i]
+};
+
+//A function that returns a random color from the variable bgColor
+function getRandomColor(){
+  var p = Math.floor(Math.random() * bgColor.length);
+  return bgColor[p];
+};
 
 
 
 
 /***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
+Function that prints out a random quote and changes the background
+color.
 ***/
+function printQuote(){
+var changeColor = getRandomColor();
+var theInfo = getRandomQuote();
+var html = '';
+html += '<p class="quote">'+ theInfo.quote + '</p>';
+html += '<p class="source">'+ theInfo.source + '';
+if (theInfo.citation){
+  html += '<span class="citation">' + theInfo.citation + '</span>';
+}
+if (theInfo.year) {
+  html += '<span class="year">' + theInfo.year + '</span>';
+}
+if (theInfo.tag) {
+  html += '<span class="year">' + theInfo.tag + '</span>';
+}
+html += '</p>';
+document.getElementById('quote-box').innerHTML = html; //info from w3school.com
+document.querySelector("body").style.background = changeColor; //info from youtube.com 'Online Tutorials'
+};
+printQuote();
 
 
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+//setInterval used to change the quote and background color every 25 seconds after the button is pressed.
+document.getElementById('loadQuote').addEventListener("click", function(){
+    setInterval(function(){
+    printQuote();
+  }, 25000);
+});
